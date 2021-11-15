0f0c3cf5cc18517f248289b1375c00498f291f47
Leia <87383466+klynmc@users.noreply.github.com>
2021-11-15 00:03:55 -0600

    edit code

--- models/index.js
+++ models/index.js
@@ -15,13 +15,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'product',
   foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'tag',
   forgeinKey: 'tag_name'
 })
 
